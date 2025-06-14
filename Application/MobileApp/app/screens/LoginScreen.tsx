4dffb6b5239120fcd88ad8fcc9c28725e38ba557
Komtufity <megachingun@gmail.com>
2025-06-14 11:58:32 +0700

    Refactor Login UI

--- Application/MobileApp/app/screens/LoginScreen.tsx
+++ Application/MobileApp/app/screens/LoginScreen.tsx
@@ -1,8 +1,8 @@
 import { observer } from "mobx-react-lite"
 import { ComponentType, FC, useEffect, useMemo, useRef, useState } from "react"
-// eslint-disable-next-line no-restricted-imports
-import { TextInput, TextStyle, ViewStyle } from "react-native"
+import { TextInput, TextStyle, ViewStyle, ImageStyle, View } from "react-native"
 import {
+  AutoImage,
   Button,
   PressableIcon,
   Screen,
@@ -14,6 +14,7 @@ import { useStores } from "../models"
 import { AppStackScreenProps } from "../navigators"
 import type { ThemedStyle } from "@/theme"
 import { useAppTheme } from "@/utils/useAppTheme"
+import { Image } from "react-native"
 
 interface LoginScreenProps extends AppStackScreenProps<"Login"> {}
 
@@ -36,8 +37,8 @@ export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_
   useEffect(() => {
     // Here is where you could fetch credentials from keychain or storage
     // and pre-fill the form fields.
-    setAuthEmail("ignite@infinite.red")
-    setAuthPassword("ign1teIsAwes0m3")
+    setAuthEmail("")
+    setAuthPassword("")
 
     // Return a "cleanup" function that React will run when the component unmounts
     return () => {
@@ -84,66 +85,87 @@ export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_
     <Screen
       preset="auto"
       contentContainerStyle={themed($screenContentContainer)}
-      safeAreaEdges={["top", "bottom"]}
+      safeAreaEdges={["bottom"]}
     >
+      <AutoImage
+        source={require("../../assets/images/intro-logo.png")}
+        maxWidth={280}
+        maxHeight={280}
+        style={themed($logoStyle)}
+      />
       <Text testID="login-heading" tx="loginScreen:logIn" preset="heading" style={themed($logIn)} />
-      <Text tx="loginScreen:enterDetails" preset="subheading" style={themed($enterDetails)} />
       {attemptsCount > 2 && (
         <Text tx="loginScreen:hint" size="sm" weight="light" style={themed($hint)} />
       )}
-
-      <TextField
-        value={authEmail}
-        onChangeText={setAuthEmail}
-        containerStyle={themed($textField)}
-        autoCapitalize="none"
-        autoComplete="email"
-        autoCorrect={false}
-        keyboardType="email-address"
-        labelTx="loginScreen:emailFieldLabel"
-        placeholderTx="loginScreen:emailFieldPlaceholder"
-        helper={error}
-        status={error ? "error" : undefined}
-        onSubmitEditing={() => authPasswordInput.current?.focus()}
-      />
-
-      <TextField
-        ref={authPasswordInput}
-        value={authPassword}
-        onChangeText={setAuthPassword}
-        containerStyle={themed($textField)}
-        autoCapitalize="none"
-        autoComplete="password"
-        autoCorrect={false}
-        secureTextEntry={isAuthPasswordHidden}
-        labelTx="loginScreen:passwordFieldLabel"
-        placeholderTx="loginScreen:passwordFieldPlaceholder"
-        onSubmitEditing={login}
-        RightAccessory={PasswordRightAccessory}
-      />
-
-      <Button
-        testID="login-button"
-        tx="loginScreen:tapToLogIn"
-        style={themed($tapButton)}
-        preset="reversed"
-        onPress={login}
-      />
+      <View style={themed($fieldsContainer)}>
+        <TextField
+          value={authEmail}
+          onChangeText={setAuthEmail}
+          containerStyle={themed($textField)}
+          autoCapitalize="none"
+          autoComplete="email"
+          autoCorrect={false}
+          keyboardType="email-address"
+          labelTx="loginScreen:emailFieldLabel"
+          placeholderTx="loginScreen:emailFieldPlaceholder"
+          helper={error}
+          status={error ? "error" : undefined}
+          onSubmitEditing={() => authPasswordInput.current?.focus()}
+        />
+        <TextField
+          ref={authPasswordInput}
+          value={authPassword}
+          onChangeText={setAuthPassword}
+          containerStyle={themed($textField)}
+          autoCapitalize="none"
+          autoComplete="password"
+          autoCorrect={false}
+          secureTextEntry={isAuthPasswordHidden}
+          labelTx="loginScreen:passwordFieldLabel"
+          placeholderTx="loginScreen:passwordFieldPlaceholder"
+          onSubmitEditing={login}
+          RightAccessory={PasswordRightAccessory}
+        />
+        <Button
+          testID="login-button"
+          tx="loginScreen:tapToLogIn"
+          style={themed($tapButton)}
+          preset="reversed"
+          onPress={login}
+        />
+      </View>
+      <View style={themed($registerPromptContainer)}>
+        <Text style={themed($registerPromptText)}>
+          Not registered?{" "}
+          <Text
+            style={themed($registerPromptLink)}
+            onPress={() => _props.navigation.navigate("Register")}
+            weight="bold"
+          >
+            Register now!
+          </Text>
+        </Text>
+      </View>
     </Screen>
   )
 })
 
 const $screenContentContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
   paddingVertical: spacing.xxl,
-  paddingHorizontal: spacing.lg,
 })
 
 const $logIn: ThemedStyle<TextStyle> = ({ spacing }) => ({
   marginBottom: spacing.sm,
 })
 
-const $enterDetails: ThemedStyle<TextStyle> = ({ spacing }) => ({
-  marginBottom: spacing.lg,
+const $logoStyle: ThemedStyle<ImageStyle> = ({ spacing }) => ({
+  alignSelf: "center",
+  marginBottom: spacing.xxs,
+  width: 280,
+  height: 180,
 })
 
 const $hint: ThemedStyle<TextStyle> = ({ colors, spacing }) => ({
@@ -157,4 +179,25 @@ const $textField: ThemedStyle<ViewStyle> = ({ spacing }) => ({
 
 const $tapButton: ThemedStyle<ViewStyle> = ({ spacing }) => ({
   marginTop: spacing.xs,
+})
+
+const $fieldsContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  width: 320,
+  maxWidth: "90%",
+  alignSelf: "center",
+})
+
+const $registerPromptContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  marginTop: spacing.xl,
+  alignItems: "center",
+})
+
+const $registerPromptText: ThemedStyle<TextStyle> = ({ colors }) => ({
+  color: colors.textDim,
+  fontSize: 15,
+})
+
+const $registerPromptLink: ThemedStyle<TextStyle> = ({ colors }) => ({
+  color: colors.tint,
+  textDecorationLine: "underline",
 })
